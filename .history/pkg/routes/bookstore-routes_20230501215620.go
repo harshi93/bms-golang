1e48ad3d0599b8cd69de9bd7b7a3fc0519cc39f5
harshi93 <singhharshit.4674@gmail.com>
2023-05-02 21:46:32 -0500

    features book mgmt sys using golang

--- .history/pkg/routes/bookstore-routes_20230501215620.go
+++
@@ -1,8 +0,0 @@
-package routes
-
-import (
-	"github.com/gorilla/mux"
-	"github.com/harshi93/go-bookstore/pkg/controllers"
-)
-
-var RegisterBookStore
--- .history/pkg/utils/utils_20230501221620.go
+++
@@ -1,6 +0,0 @@
-/* this file will be responsible
-for marshalling and unmarshalling
-of json payload sent to user and
-received from user
-*/
-
